a0e7746b5ecac44f6dcdb9b6fa7b78e064a24df0
huynguyen2741 <33584122+huynguyen2741@users.noreply.github.com>
2022-09-14 11:31:48 -0400

    Update my-app-config.ts

--- ecommerce-frontend/src/app/config/my-app-config.ts
+++ ecommerce-frontend/src/app/config/my-app-config.ts
@@ -1,16 +1,17 @@
+[11:30 AM] Joshua Craven
 export default {
-  oidc: {
-    clientId: '0oa6ab9e99dOuneyG5d7',
-
-    issuer: 'https://dev-08324756.okta.com/oauth2/default',
-
-    redirectUri: 'http://localhost:4200/home',
-
-    postLogoutRedirectUri: 'http://localhost:4200/login',
-
-    scopes: ['openid', 'profile', 'email'],
-  },
-};
-
+    oidc: {
+        clientId: '0oa6hnwwonsOm1f6n5d7',
+        issuer: 'https://dev-3553715.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/home',
+        postLogoutRedirectUri: 'http://localhost:4200/login',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
+// OLD CREDENTIALS
 // '0oa6ab9e99dOuneyG5d7
 // https://dev-08324756.okta.com/oauth2/default
+// NEW CREDENTIALS WITH WORKING REGISTER
+// '0oa6hnwwonsOm1f6n5d7'
+// https://dev-3553715.okta.com/oauth2/default
+
